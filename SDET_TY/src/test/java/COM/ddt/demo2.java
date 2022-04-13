6ccfe887b1178873c99c5c37c4f2aa1d69c5c55f
Kalyankar Shashank <Kalyankar Shashank@Kranthi-Z580>
2022-04-13 18:04:08 +0530

    Demo 2 push

---
+++ SDET_TY/src/test/java/COM/ddt/demo2.java
@@ -0,0 +1,14 @@
+package COM.ddt;
+
+public class demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		
+		//xpath
+		//cssselector
+		//my name is shashank 
+	}
+
+}
