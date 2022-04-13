cf1597f411bcceeec71c16906a158118a0859721
Kalyankar Shashank <Kalyankar Shashank@Kranthi-Z580>
2022-04-13 18:24:10 +0530

    demo1class

---
+++ SDET_TY/src/test/java/COM/ddt/demo1.java
@@ -0,0 +1,18 @@
+package COM.ddt;
+
+import org.openqa.selenium.By;
+
+import com.mysql.cj.jdbc.Driver;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		/**
+		 * 
+		 */
+	//xpath=cssselector
+		//name="shashank"
+		
+	}
+
+}
