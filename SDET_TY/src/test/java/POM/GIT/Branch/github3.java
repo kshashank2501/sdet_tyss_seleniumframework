bce949012dec7f25e8df85c8962d3e84d6c91502
Kalyankar Shashank <Kalyankar Shashank@Kranthi-Z580>
2022-04-14 12:08:23 +0530

    2 Other source code is pushed in git

---
+++ SDET_TY/src/test/java/POM/GIT/Branch/github3.java
@@ -0,0 +1,14 @@
+package POM.GIT.Branch;
+
+public class github3 {
+
+	public static void main(String[] args) {
+		{
+			
+			 System.out.println("Hello world");  
+		 
+				}  
+
+	}
+
+}
---
+++ SDET_TY/src/test/java/POM/GIT/Branch/github4.java
@@ -0,0 +1,14 @@
+package POM.GIT.Branch;
+
+import org.testng.annotations.Test;
+
+public class github4 {
+	@Test
+	public void git()
+	{
+		
+	 System.out.println("Hello Java");  
+ 
+		}  
+
+}
