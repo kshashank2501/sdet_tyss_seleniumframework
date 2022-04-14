0e4f8cb10002af9d48dce522078ebb4ccb67228c
Kalyankar Shashank <Kalyankar Shashank@Kranthi-Z580>
2022-04-14 11:55:11 +0530

    New Git package

---
+++ SDET_TY/src/test/java/POM/GIT/Branch/Github1.java
@@ -0,0 +1,20 @@
+package POM.GIT.Branch;
+
+public class Github1 {
+
+	public static void main(String[] args) {
+		int a[]= {100,200,300,400,500};
+		int sum=0;
+		
+		for (int i:a)
+		{
+			System.out.println(i);
+			sum=sum+i;
+			//0+100=100; 100+200=300;
+			System.out.println(sum);
+			}
+
+
+	}
+
+}
