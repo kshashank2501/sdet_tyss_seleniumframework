69f7fcaa7e1cee01ba31c89f61e1f64f483df8fd
Kalyankar Shashank <Kalyankar Shashank@Kranthi-Z580>
2022-04-14 12:22:54 +0530

    New GitHUbCOde

---
+++ SDET_TY/src/test/java/POM/GIT/Branch/Github2.java
@@ -0,0 +1,21 @@
+package POM.GIT.Branch;
+
+public class Github2 {
+
+	public static void main(String[] args) {
+		int b[]= {100,200,300,400,500};
+		int sum=0;
+		
+		for (int i:b)
+		{
+			System.out.println(i);
+			sum=sum+i;
+			//0+100=100; 100+200=300;
+			System.out.println(sum);
+			}
+	}
+
+
+	}
+
+
